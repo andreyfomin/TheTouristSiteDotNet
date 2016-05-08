ad3d1a7fc0c10c1baa8ce9d81c200d9a2bc9cbd5
Andrey <fomin.andre@gmail.com>
2016-05-08 16:04:36 +0300

    update manage tours
    update manage tours

--- ManageTours.aspx
+++ ManageTours.aspx
@@ -1,27 +1,44 @@
 ﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ManageTours.aspx.cs" Inherits="ManageTours" %>
 
+
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
+ <link rel="Stylesheet" href="StyleSheet2.css" />
     <title></title>
 </head>
 <body>
+
     <form id="form1" runat="server">
     <div>
-        <asp:DropDownList ID="DropDownListTours" runat="server" onclick="DropDownListTours_Click">
+        
+        <h1 style="Text-align:center;position:absolute;top:0px;left:50px">Tours Manager</h1>
+        <img src="Images/romanage.jpg" alt="logo" width="100%" height="350" top="0">
+        <div class="rt-delete" style="position:absolute;width=700px;margin-left:400px">
+        <h2>Delete tour</h2>
+        <asp:DropDownList ID="DropDownListTours" runat="server" >
         </asp:DropDownList>
         <asp:Button ID="Delete" runat="server" Text="delete tour" OnClick="AdminDeleteTour_Click" />
-    </div>
-    <div class="updateorinserttours">
-        <asp:Label ID="LabelName" runat="server" Text="name"></asp:Label>
-        <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
-        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
-        <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
-        <asp:FileUpload ID="FileUpload1" runat="server" />
-        <asp:Button ID="Add" runat="server" Text="Add New tour" OnClick="AdminAddTour_Click" />
-    </div>
-    <p>
-        &nbsp;</p>
-    </form>
+        </div>
+   
+    <div class="updateorinserttours" style="margin-left:700px">
+    <h3>Add new tour</h3>
+        <asp:Label ID="LabelName" runat="server" style="font-weight:bold">Tour Name</asp:Label>
+         <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        <br /><br />
+       <asp:Label ID="LabelDescription" runat="server" style="font-weight:bold">Description</asp:Label>
+       <textarea id="TextArea1" runat="server" cols="90" rows="14"></textarea>
+      <br /><br />
+        <asp:Label ID="LabelImage" runat="server" style="font-weight:bold">Upload image</asp:Label>
+        <asp:FileUpload ID="FileUpload1" runat="server"  />
+       
+
+        <asp:Button ID="Add" runat="server" Text="Add New tour" onclick="AdminAddTour_Click"/>
+
+        </div>
+        <asp:LinkButton ID="LinkButtonMain" runat="server" OnClick="LinkButtonMain_Click" CssClass="buttonClass" >Return to main page</asp:LinkButton>
+      </div>
+   </form>
 </body>
 </html>
--- ManageTours.aspx.cs
+++ ManageTours.aspx.cs
@@ -9,9 +9,9 @@ using System.Web.Configuration;
 using System.Data.SqlClient;
 
 
-
 public partial class ManageTours : System.Web.UI.Page
 {
+
     string connectionString = WebConfigurationManager.ConnectionStrings["ToursDB"].ConnectionString;
 
 
@@ -64,28 +64,6 @@ public partial class ManageTours : System.Web.UI.Page
         }
     }
 
-    protected void DropDownListTours_Click(object sender, EventArgs e)
-    {
-
-        ArrayList tours = (ArrayList)this.Application["tours"];
-        Tour selectedTour = null;
-
-        foreach (Tour tour in tours)
-        {
-            if (DropDownListTours.SelectedValue != null)
-                if (tour.Id == DropDownListTours.SelectedIndex + 2)
-                {
-                    selectedTour = tour;
-                }
-        }
-
-        if (selectedTour != null)
-        {
-            Session["userSelectedTour"] = selectedTour;
-        }
-
-    }
-
 
     public static Control[] FlattenHierachy(Control root)
     {
@@ -131,25 +109,25 @@ public partial class ManageTours : System.Web.UI.Page
     protected void AdminAddTour_Click(object sender, EventArgs e)
     {
 
-        if (FileUpload1.HasFile && !String.IsNullOrEmpty(TextBox1.Text) && !String.IsNullOrEmpty(TextBox2.Text))
+        if (FileUpload1.HasFile && !String.IsNullOrEmpty(TextBox1.Text) && !String.IsNullOrEmpty(TextArea1.Value))
         {
             ArrayList tours = (ArrayList)Application["tours"];
             string str = FileUpload1.FileName;
-            FileUpload1.PostedFile.SaveAs(Server.MapPath(".") + "/images/" + str);
-            string path = "images/" + str.ToString();
+            FileUpload1.PostedFile.SaveAs(Server.MapPath(".") + "/Images/" + str);
+            string path = "Images/" + str.ToString();
             SqlConnection con = new SqlConnection(connectionString);
 
             if (tours != null)
             {
                 Tour KnownTour = Array.Find((Tour[])tours.ToArray(typeof(Tour)),
-                         tour => tour.Info.Equals(TextBox1.Text) || tour.Description.Equals(TextBox2.Text) || tour.Image.Equals(path));
+                         tour => tour.Info.Equals(TextBox1.Text) || tour.Description.Equals(TextArea1.Value) || tour.Image.Equals(path));
                 if (KnownTour == null)
                 {
                     con.Open();
                     SqlCommand cmd = new SqlCommand("insert into tours (info,image,description) values (@info,@image,@description)", con);
                     cmd.Parameters.AddWithValue("@info", TextBox1.Text);
                     cmd.Parameters.AddWithValue("@image", path);
-                    cmd.Parameters.AddWithValue("@description", TextBox2.Text);
+                    cmd.Parameters.AddWithValue("@description", TextArea1.Value);
 
                     cmd.ExecuteNonQuery();
                     con.Close();
@@ -168,7 +146,7 @@ public partial class ManageTours : System.Web.UI.Page
                     // Add the parameters.
                     cmd.Parameters.AddWithValue("@info", TextBox1.Text);
                     cmd.Parameters.AddWithValue("@image", path);
-                    cmd.Parameters.AddWithValue("@description", TextBox2.Text);
+                    cmd.Parameters.AddWithValue("@description", TextArea1.Value);
                     cmd.Parameters.AddWithValue("@tour_id", KnownTour.Id);
 
                     // Try to open database and execute the update.
@@ -194,75 +172,78 @@ public partial class ManageTours : System.Web.UI.Page
         }
     }
 
-
-
-
     protected void AdminDeleteTour_Click(object sender, EventArgs e)
     {
-        if (Session["userSelectedTour"] != null)
+
+        Tour selectedTour = null;
+        ArrayList tours = (ArrayList)this.Application["tours"];
+
+
+
+        foreach (Tour tour in tours)
         {
-            Tour userSelectedTour = (Tour)Session["userSelectedTour"];
-            string deleteTourFromSelectedSQL;
-            deleteTourFromSelectedSQL = "DELETE FROM user_selected_tours ";
-            deleteTourFromSelectedSQL += " WHERE @tour_id=tour_id";
+            if (DropDownListTours.SelectedValue != null)
+                if (tour.Id == Decimal.Parse(DropDownListTours.SelectedItem.Value))
+                {
+                    selectedTour = tour;
+                }
+        }
 
-            SqlConnection con = new SqlConnection(connectionString);
-            SqlCommand cmd = new SqlCommand(deleteTourFromSelectedSQL, con);
+        string deleteTourFromSelectedSQL;
+        deleteTourFromSelectedSQL = "DELETE FROM user_selected_tours ";
+        deleteTourFromSelectedSQL += " WHERE tour_id=@tour_id";
 
-            cmd.Parameters.AddWithValue("@tour_id", userSelectedTour.Id);
+        SqlConnection con = new SqlConnection(connectionString);
+        SqlCommand cmd = new SqlCommand(deleteTourFromSelectedSQL, con);
 
-            int removed = 0;
-            try
-            {
-                con.Open();
-                removed = cmd.ExecuteNonQuery();
+        cmd.Parameters.AddWithValue("@tour_id", selectedTour.Id);
+        // cmd.Parameters.AddWithValue("@user_id", ((User)Session["logedInUser"]).Id);
 
-            }
-            catch (Exception err)
-            {
-
-            }
-            finally
-            {
-                con.Close();
-            }
-            ClearDropDownLists();
-            ClearTextBoxes();
-
-            string deleteTourSQL;
-            deleteTourSQL = "DELETE FROM tours ";
-            deleteTourSQL += " WHERE @info=info";
-
-            SqlConnection conn = new SqlConnection(connectionString);
-            SqlCommand comd = new SqlCommand(deleteTourSQL, conn);
-
-            comd.Parameters.AddWithValue("@info", userSelectedTour.Info);
-            comd.Parameters.AddWithValue("@image", userSelectedTour.Image);
-            comd.Parameters.AddWithValue("@description", userSelectedTour.Description);
-
-
-            int removed2 = 0;
-            try
-            {
-                con.Open();
-                removed2 = cmd.ExecuteNonQuery();
-
-            }
-            catch (Exception err)
-            {
-
-            }
-            finally
-            {
-                con.Close();
-            }
-            ClearDropDownLists();
-            ClearTextBoxes();
-
-
+        int removed = 0;
+        try
+        {
+            con.Open();
+            removed = cmd.ExecuteNonQuery();
 
         }
+        catch (Exception err)
+        {
+
+        }
+        finally
+        {
+            con.Close();
+        }
+        ClearDropDownLists();
+        ClearTextBoxes();
+
+        string deleteTourSQL;
+        deleteTourSQL = "DELETE FROM tours WHERE id=@id";
+
+        SqlConnection conn = new SqlConnection(connectionString);
+        SqlCommand comd = new SqlCommand(deleteTourSQL, conn);
+
+        comd.Parameters.AddWithValue("@id ", selectedTour.Id);
+        int removed2 = 0;
+        try
+        {
+            con.Open();
+            removed2 = cmd.ExecuteNonQuery();
+
+        }
+        catch (Exception err)
+        {
+
+        }
+        finally
+        {
+            con.Close();
+        }
+        ClearDropDownLists();
+        ClearTextBoxes();
     }
-
-
+    protected void LinkButtonMain_Click(object sender, EventArgs e)
+    {
+        Server.Transfer("~/Default.aspx");
+    }
 }
